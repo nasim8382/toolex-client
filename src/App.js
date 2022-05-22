a94c7c1f5b2d5646bb1c45cb819c4df97a530c9f
Nasim Reza <nasim8382@gmail.com>
2022-05-23 00:54:47 +0600

    google font added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-body'>
+      <h1 className='text-5xl text-center mt-20 font-bold'>My Name is Nasim</h1>
+      <h2 className='text-2xl my-5 text-center font-semibold'>I live in Gangni</h2>
+      <button class="btn btn-secondary block mx-auto">Button</button>
     </div>
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        toolextheme: {
+          primary: "#172B34",
+          secondary: "#fdb819",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+      "dark",
+      "cupcake",
+    ],
+  },
+  plugins: [require("daisyui")],
+};
