a0f9214ed8fceefb54022ba7f29171838311279e
Nasim Reza <nasim8382@gmail.com>
2022-05-23 01:30:34 +0600

    project added with firebase

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import './App.css';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
@@ -6,6 +8,7 @@ function App() {
       <h1 className='text-5xl text-center mt-20 font-bold'>My Name is Nasim</h1>
       <h2 className='text-2xl my-5 text-center font-semibold'>I live in Gangni</h2>
       <button class="btn btn-secondary block mx-auto">Button</button>
+      <ToastContainer />
     </div>
   );
 }
