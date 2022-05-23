3225839de9217c3b2227090e38d60a6461b06cb6
Nasim Reza <nasim8382@gmail.com>
2022-05-23 10:56:35 +0600

    banner section added

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,12 +1,25 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import banner from '../../images/banner.jpg';
 
 const Banner = () => {
-    return (
+  return (
+    <div
+      className="hero min-h-screen"
+      style={{background: `url(${banner})`, backgroundRepeat: 'no-repeat', backgroundPosition: 'Center', backgroundSize: 'cover'}}
+    >
+        <div className="hero-overlay bg-opacity-60"></div>
+      <div className="hero-content text-center">
         <div>
-            <h1 className='text-5xl text-center mt-20 font-bold text-secondary'>This is banner page</h1>
-            <button className="cursor-pointer rounded-md bg-secondary px-4 py-3 text-center text-sm font-bold uppercase text-gray-700 transition duration-200 ease-in-out hover:bg-primary hover:text-white block mx-auto">Button</button>
+            <h2 className="mb-7 text-3xl md:text-5xl font-bold text-secondary">Free Shipping For</h2>
+          <h1 className="mb-10 text-5xl md:text-7xl font-bold text-white">Best Mechanic Tool Set</h1>
+          <Link to="/blogs">
+            <button className="cursor-pointer rounded-md bg-secondary px-4 py-3 text-center text-sm font-bold uppercase text-gray-700 transition duration-200 ease-in-out hover:bg-primary hover:text-white block mx-auto">Our Blogs</button>
+          </Link>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div>
-            <h1 className='text-5xl text-center mt-20 font-bold text-secondary'>This is home page</h1>
+            <Banner></Banner>
         </div>
     );
 };
